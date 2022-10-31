748dfb47dabd0c54d205106562172bef4bb246c5
Babatunde Alli <babatunde.alli@mail.mcgill.ca>
2022-10-30 21:57:35 -0400

    Create ruby_script_generator.rb

---
+++ ruby_script_generator.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#This Ruby script will create a new ruby file, add a shebang line at the top, makes it executable
+#then open it in a new VS Code window
+path = ARGV[0]
+fail "specify filename to create" unless path
+File.open(path, "w") { |f| f.puts "#!/usr/bin/env ruby" }
+File.chmod(0755, path) #make the file executable
+system ("code", path) #opens the file in a new VS Code window
