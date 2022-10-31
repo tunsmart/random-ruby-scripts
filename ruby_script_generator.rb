dc95a609defc210c26a504adc493eecac250adf0
Babatunde Alli <babatunde.alli@mail.mcgill.ca>
2022-10-30 21:58:24 -0400

    Update ruby_script_generator.rb

--- ruby_script_generator.rb
+++ ruby_script_generator.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+#Executable ruby file creation automation
 #This Ruby script will create a new ruby file, add a shebang line at the top, makes it executable
 #then open it in a new VS Code window
 path = ARGV[0]
